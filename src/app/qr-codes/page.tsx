5e3911e0a7928ba4c85a3c4f8c366a25ed0d84e2
Nevin Alcober <nevinvinz@gmail.com>
2026-07-19 21:05:26 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@/

--- src/app/qr-codes/page.tsx
+++ src/app/qr-codes/page.tsx
@@ -32,7 +32,7 @@ import {
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Badge } from "@/components/ui/badge";
-import { Avatar, AvatarFallback, AvatarImage } from "@/avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Separator } from "@/components/ui/separator";
 import { Checkbox } from "@/components/ui/checkbox";
 import {
